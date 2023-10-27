cf01634e0057501095e52b5af7ad88fb4dd57f55
Márcia Aquino <marciaatule@gmail.com>
2023-10-26 22:05:35 -0300

    criação da tabela lista

--- bd e github.sql
+++ bd e github.sql
@@ -1,3 +1,12 @@
 -- criação banco de dados contato
 CREATE DATABASE contato;
 
+-- utilizar o comando
+USE contato;
+
+-- criar uma tabela chamada Lista com 3 campos 
+CREATE TABLE Lista (
+nome VARCHAR (50) NOT NULL,
+ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+Telefone VARCHAR (11) NOT NULL
+);
