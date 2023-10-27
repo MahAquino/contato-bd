c88336c8b9e32b010f35c4f11a1ad8eea19b131f
Márcia Aquino <marciaatule@gmail.com>
2023-10-26 22:56:57 -0300

    Inclusão do UF nos funcionarios

--- bd e github.sql
+++ bd e github.sql
@@ -8,12 +8,14 @@ USE contato;
 CREATE TABLE Lista (
 ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 nome VARCHAR (50) NOT NULL,
-Telefone VARCHAR (11) NOT NULL
+Telefone VARCHAR (11) NOT NULL,
+UF VARCHAR (2) NOT NULL
 );
 
 -- inserir um contato na tabela Lista
-INSERT INTO Lista (nome, Telefone) VALUES 
-	('Faustino Manuel', '11987390490'),
-	('Verena Aquino', '11965024488'),
-	('Heyke Cabezas', '11925698511');
+INSERT INTO Lista (nome, Telefone, UF) VALUES 
+	('Faustino Manuel', '11987390490', 'SP'),
+	('Verena Aquino', '11965024488', 'MG'),
+	('Heyke Cabezas', '11925698511', 'DF');
     
+    -- Escrever novo comando para apresentar o total de 
