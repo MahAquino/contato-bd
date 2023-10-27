529dbc7ec52b72b614f0a7e6fce18c1df4bf563e
Márcia Aquino <marciaatule@gmail.com>
2023-10-26 22:30:14 -0300

    Inclusão dos contatos na tabela Lista

--- bd e github.sql
+++ bd e github.sql
@@ -10,3 +10,10 @@ nome VARCHAR (50) NOT NULL,
 ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 Telefone VARCHAR (11) NOT NULL
 );
+
+-- inserir um contato na tabela Lista
+INSERT INTO Lista (nome, Telefone) VALUES 
+	('Faustino Manuel', '11987390490'),
+	('Verena Aquino', '11965024488'),
+	('Heyke Cabezas', '11925698511');
+    
